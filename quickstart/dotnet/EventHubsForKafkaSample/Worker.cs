e05368741ac70932fa6567329e72befb31d5bfed
Bill Berry <WilliamBerryiii@users.noreply.github.com>
2018-10-18 19:42:13 -0700

    Add "auto.offset.reset" sample value to consumer config.

--- quickstart/dotnet/EventHubsForKafkaSample/Worker.cs
+++ quickstart/dotnet/EventHubsForKafkaSample/Worker.cs
@@ -26,7 +26,7 @@ namespace EventHubsForKafkaSample
                     { "sasl.mechanism", "PLAIN" },
                     { "sasl.username", "$ConnectionString" },
                     { "sasl.password", connStr },
-                    { "ssl.ca.location", cacertlocation }
+                    { "ssl.ca.location", cacertlocation },
                     //{ "debug", "security,broker,protocol" }       //Uncomment for librdkafka debugging information
                 };
 
@@ -58,6 +58,7 @@ namespace EventHubsForKafkaSample
                     { "ssl.ca.location", cacertlocation },
                     { "group.id", consumergroup },
                     { "request.timeout.ms", 60000 },
+                    //{ "auto.offest.reset", "earliest" },          //Uncomment for setting offest value, valid args: [latest, earliest, none]
                     //{ "debug", "security,broker,protocol" }       //Uncomment for librdkafka debugging information
                 };
 
