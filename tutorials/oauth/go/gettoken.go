2f12af36d2c9ee7e56187cea814afcf6a295f9e2
Serkant Karaca <serkar@microsoft.com>
2019-11-27 15:10:25 -0800

    -

--- tutorials/oauth/go/gettoken.go
+++ tutorials/oauth/go/gettoken.go
@@ -9,8 +9,37 @@ package main
 
 import (
 	"fmt"
+	"github.com/Azure/go-autorest/autorest"
+	"github.com/Azure/go-autorest/autorest/adal"
+	"github.com/Azure/go-autorest/autorest/azure"
+	"github.com/Azure/go-autorest/autorest/date"
 )
 
 func main() {
 	fmt.Println("hello world");
+
+	applicationID := "fd756bd0-6853-41fd-8f5a-cc7e8bd89214"
+	applicationSecret := "l.vUN-4SRE45Ph9@D-]_s[zUaOBchc-i"
+	tenantID := "72f988bf-86f1-41af-91ab-2d7cd011db47"
+
+	const activeDirectoryEndpoint = "https://login.microsoftonline.com/"
+	oauthConfig, err := adal.NewOAuthConfig(activeDirectoryEndpoint, tenantID)
+
+
+	spt, err := adal.NewServicePrincipalToken(
+		*oauthConfig,
+		appliationID,
+		applicationSecret,
+		resource,
+		callbacks...)
+	if err != nil {
+		return nil, err
+	}
+	
+	// Acquire a new access token
+	err  = spt.Refresh()
+	if (err == nil) {
+		token := spt.Token
+		fmt.Println("Token " + token);
+	}
 }
