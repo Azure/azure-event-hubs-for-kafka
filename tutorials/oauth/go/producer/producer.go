5b020a3ea1425356dcba9b28a9414cb67a34b0dc
Serkant Karaca <serkar@microsoft.com>
2019-11-30 21:40:30 -0800

    -

--- tutorials/oauth/go/producer/producer.go
+++ tutorials/oauth/go/producer/producer.go
@@ -89,18 +89,18 @@ func main() {
 	}(p.Events())
 
 	// Delivery report handler for produced messages
-	go func() {
-		for e := range p.Events() {
-			switch ev := e.(type) {
-			case *kafka.Message:
-				if ev.TopicPartition.Error != nil {
-					fmt.Printf("Delivery failed: %v\n", ev.TopicPartition)
-				} else {
-					fmt.Printf("Delivered message to %v\n", ev.TopicPartition)
-				}
-			}
-		}
-	}()
+	// go func() {
+	// 	for e := range p.Events() {
+	// 		switch ev := e.(type) {
+	// 		case *kafka.Message:
+	// 			if ev.TopicPartition.Error != nil {
+	// 				fmt.Printf("Delivery failed: %v\n", ev.TopicPartition)
+	// 			} else {
+	// 				fmt.Printf("Delivered message to %v\n", ev.TopicPartition)
+	// 			}
+	// 		}
+	// 	}
+	// }()
 
 	// Produce messages to topic (asynchronously)
 	topic := "test"
