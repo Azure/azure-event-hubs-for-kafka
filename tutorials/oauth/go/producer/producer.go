b81946ae490d8e64703744bf4237e3af516c5a63
Serkant Karaca <serkar@microsoft.com>
2019-11-30 21:13:58 -0800

    -

--- tutorials/oauth/go/producer/producer.go
+++ tutorials/oauth/go/producer/producer.go
@@ -10,22 +10,83 @@ package main
 import (
 	"fmt"
 	"os"
+	"time"
+
 	"github.com/confluentinc/confluent-kafka-go/kafka"
 )
 
+func handleOAuthBearerTokenRefreshEvent(client kafka.Handle, e kafka.OAuthBearerTokenRefresh) {
+	fmt.Println("handleOAuthBearerTokenRefreshEvent")
+	oauthBearerToken, retrieveErr := retrieveToken(e)
+	if retrieveErr != nil {
+		fmt.Fprintf(os.Stderr, "%% Token retrieval error: %v\n", retrieveErr)
+		client.SetOAuthBearerTokenFailure(retrieveErr.Error())
+	} else {
+		setTokenError := client.SetOAuthBearerToken(oauthBearerToken)
+		if setTokenError != nil {
+			fmt.Fprintf(os.Stderr, "%% Error setting token and extensions: %v\n", setTokenError)
+			client.SetOAuthBearerTokenFailure(setTokenError.Error())
+		}
+	}
+}
+
+func retrieveToken(e kafka.OAuthBearerTokenRefresh) (kafka.OAuthBearerToken, error) {
+	fmt.Println("in retrieveToken")
+
+	now := time.Now()
+	nowSecondsSinceEpoch := now.Unix()
+
+	// The token lifetime needs to be long enough to allow connection and a broker metadata query.
+	// We then exit immediately after that, so no additional token refreshes will occur.
+	// Therefore set the lifetime to be an hour (though anything on the order of a minute or more
+	// would be fine).
+	// expiration := now.Add(time.Second * time.Duration(3600))
+	// expirationSecondsSinceEpoch := expiration.Unix()
+
+	// oauthbearerMapForJSON := map[string]interface{}{
+	// 	principalClaimName: principal,
+	// 	"iat":              nowSecondsSinceEpoch,
+	// 	"exp":              expirationSecondsSinceEpoch,
+	// }
+	// claimsJSON, _ := json.Marshal(oauthbearerMapForJSON)
+	// encodedClaims := base64.RawURLEncoding.EncodeToString(claimsJSON)
+	// jwsCompactSerialization := joseHeaderEncoded + "." + encodedClaims + "."
+	// extensions := map[string]string{}
+	// oauthBearerToken := kafka.OAuthBearerToken{
+	// 	TokenValue: jwsCompactSerialization,
+	// 	Expiration: expiration,
+	// 	Principal:  principal,
+	// 	Extensions: extensions,
+	// }
+	return oauthBearerToken, nil
+}
+
 func main() {
-	// https://github.com/edenhill/librdkafka/blob/master/CONFIGURATION.md
-	p, err := kafka.NewProducer(&kafka.ConfigMap{
+	config := kafka.ConfigMap{
 		"bootstrap.servers": os.Getenv("KAFKA_EVENTHUB_ENDPOINT"),
-		"sasl.mechanisms":   "PLAIN",
 		"security.protocol": "SASL_SSL",
-		"sasl.username":     "$ConnectionString",
-		"sasl.password":     os.Getenv("KAFKA_EVENTHUB_CONNECTION_STRING"),
-	})
+		"sasl.mechanisms":   "OAUTHBEARER",
+	}
+
+	p, err := kafka.NewProducer(&config)
 
 	if err != nil {
 		panic(err)
 	}
+
+	// Token refresh events are posted on the Events channel, instructing
+	// the application to refresh its token.
+	go func(eventsChan chan kafka.Event) {
+		for ev := range eventsChan {
+			oart, ok := ev.(kafka.OAuthBearerTokenRefresh)
+			if !ok {
+				// Ignore other event types
+				continue
+			}
+
+			handleOAuthBearerTokenRefreshEvent(p, oart)
+		}
+	}(p.Events())
 
 	// Delivery report handler for produced messages
 	go func() {
